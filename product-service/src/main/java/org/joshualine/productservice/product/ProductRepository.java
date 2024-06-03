610cb932b01c82b2a4cd12bcfa1f3cf8344fb010
Joshua Chinwendu <joshuachinwendu@gmail.com>
2024-06-03 15:04:49 +0100

    Transform the Product class to an Entity

--- product-service/src/main/java/org/joshualine/productservice/product/ProductRepository.java
+++ product-service/src/main/java/org/joshualine/productservice/product/ProductRepository.java
@@ -1,63 +1,7 @@
 package org.joshualine.productservice.product;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-@Repository
-public class ProductRepository {
-
-    private final List<Product> products = new ArrayList<>();
-
-    //GET
-    List<Product> getAllProducts(){
-        return products;
-    }
-
-    Optional<Product> getProductById(Integer id){
-        return products.stream().filter(product -> product.productID().equals(id)).findFirst();
-    }
-
-    //POST
-    void createProduct(Product product){
-        products.add(product);
-    }
-
-    //UPDATE
-    void updateProduct(Product product, Integer id){
-        Optional<Product> existingProduct = getProductById(id);
-
-        if (existingProduct.isEmpty()) {
-            return;
-        }
-        products.set(products.indexOf(existingProduct.get()), product);
-    }
-    //DELETE
-    void deleteProduct(Integer id){
-        products.removeIf(product -> product.productID().equals(id));
-    }
-
-    @PostConstruct
-    void init(){
-        products.add( new Product(
-                1,
-                "Iphone",
-                "This is the first product",
-                200,
-                LocalDateTime.now(),
-                LocalDateTime.now().plusMinutes(30))
-        );
-        products.add( new Product(
-                2,
-                "Iphone2",
-                "This is the second product",
-                400,
-                LocalDateTime.now(),
-                LocalDateTime.now().plusMinutes(90))
-        );
-    }
 }
--- product-service/src/main/java/org/joshualine/productservice/product/ProductSpringDataJpaRepository.java
+++
@@ -1,9 +0,0 @@
-//package org.joshualine.productservice.product;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//
-//@Repository
-//public interface ProductSpringDataJpaRepository extends JpaRepository<Product, Integer> {
-//}
